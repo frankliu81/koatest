8303f503deb3ab170a5fe2f278eab52d847617dd
Frank Liu Mac <frankliu81@gmail.com>
2016-07-03 09:31:45 -0700

    Add a sample mocha unit test

--- helpers/utilityHelpers.js
+++ helpers/utilityHelpers.js
@@ -1,3 +1,4 @@
+// return the index within an array with attr that matches the given value
 exports.findWithAttr = function(array, attr, value) {
     for(var i = 0; i < array.length; i += 1) {
         if(array[i][attr] === value) {
